425e70b978c4a91839db8f947afc754d47663aa9
Mojeeto <mojtaba01nouri@gmail.com>
2022-09-18 12:08:38 +0430

    feat: fix problem with input comment and emoji picker

--- src/components/CommentInput.tsx
+++ src/components/CommentInput.tsx
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { useContext, useState, useRef } from "react";
 import Picker from "emoji-picker-react";
 import { MdOutlineTagFaces } from "react-icons/md";
 import { TodoModalContext } from "./TodoModalContext";
-import { CommentNeededType, InputPickerStateProps, TaskDataType } from "./Types";
+import {
+  CommentNeededType,
+  InputPickerStateProps,
+  TaskDataType,
+} from "./Types";
 
-
-const CommentInput: React.FC<InputPickerStateProps> = ({ pickerInputState, setPickerInputState }) => {
-  const [data, update] = React.useContext(TodoModalContext);
-  const [inputValue, setInputValue] = React.useState("");
-  const formRef = React.useRef<HTMLFormElement>(null);
+const CommentInput: React.FC<InputPickerStateProps> = ({
+  pickerInputState,
+  setPickerInputState,
+}) => {
+  const [data, update] = useContext(TodoModalContext);
+  const [inputValue, setInputValue] = useState("");
+  const formRef = useRef<HTMLFormElement>(null);
 
   const onChangeValue: React.ChangeEventHandler<HTMLTextAreaElement> = (e) => {
     const target = e.currentTarget;
+    target.style.height = '0px';
+    target.style.height = target.scrollHeight + "px";
     const newValue = target.value;
     setInputValue(newValue);
   };
 
   const setEmojiPickerState = (newPickerState: boolean) => {
     const comments = data!.comments;
-    comments.forEach(comment => comment.pickerShow = false);
+    comments.forEach((comment) => (comment.pickerShow = false));
     const newState = {
       ...data!,
-      comments: comments
-    }
+      comments: comments,
+    };
     update!(newState);
     setPickerInputState(newPickerState);
-  }
+  };
 
   const onSubmit: React.ChangeEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
@@ -50,7 +58,7 @@ const CommentInput: React.FC<InputPickerStateProps> = ({ pickerInputState, setPi
 
   return (
     <form
-      className="flex bg-white justify-between items-center px-2 py-1 border-[1px] gap-2 rounded-md relative animate-opacity select-none"
+      className="flex bg-white justify-between w-[272px] bottom-2 items-center px-2 py-1 border-[1px] gap-2 rounded-md absolute animate-opacity select-none"
       style={{ animationDelay: "1s" }}
       onSubmit={onSubmit}
       name="comment-id"
@@ -60,13 +68,14 @@ const CommentInput: React.FC<InputPickerStateProps> = ({ pickerInputState, setPi
         value={inputValue}
         onChange={onChangeValue}
         placeholder="Type something..."
-        className="outline-none flex-1 text-slate-600 resize-none scroll-smooth"
         rows={1}
+        className="outline-none flex-1 text-slate-600 resize-none scroll-smooth overflow-hidden"
         onKeyUp={(e) => {
           if (e.key === "Enter" && !e.shiftKey) {
             const form = formRef.current;
             if (form) {
               form.requestSubmit();
+              e.currentTarget.style.height = "24px";
             }
           } else if (e.key === "Enter" && e.shiftKey) {
             setInputValue(inputValue + "\n");
@@ -74,7 +83,7 @@ const CommentInput: React.FC<InputPickerStateProps> = ({ pickerInputState, setPi
           }
         }}
         form="comment-id"
-        maxLength={32}
+        maxLength={150}
       />
       <MdOutlineTagFaces
         className="text-slate-400 cursor-pointer"
